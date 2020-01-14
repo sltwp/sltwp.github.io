f305df2453dc1623e49ecea851ae9003bcbdce25
Simon Sotak <simon.sotak@sprinx.com>
2020-01-14 14:58:03 +0100

    TODO: Add URLs to images

--- updateroster.js
+++ updateroster.js
@@ -1,5 +1,40 @@
 const tinify = require("tinify");
 tinify.key = "75b5zNHlx5l6SvXw8xQ1yxfmTz8wgKpv";
 
+/*Simon*/
 const source = tinify.fromUrl("https://tinypng.com/images/panda-happy.png");
 source.toFile("roster/optimized.jpg");
+
+/*Josh*/
+
+/*Jim*/
+
+/*Anvarne*/
+
+/*Mikey*/
+
+/*Mook*/
+
+/*Cade*/
+
+/*Cirx*/
+
+/*Orvos*/
+
+/*Nomadian*/
+
+/*Soise*/
+
+/*Adamwlock*/
+
+/*Peow*/
+
+/*Listad*/
+
+/*Spots*/
+
+/*Vegard*/
+
+/*Tom*/
+
+/*Jinkkz*/
