8eb60e8ea02d2b74e37412c7c60ae4365d5d4eb0
Simon <soltiman@gmail.com>
2020-01-16 00:25:28 +0100

    Fixed avatar scaling

--- updateroster.js
+++ updateroster.js
@@ -1,2 +1,78 @@
-var tinify.key = config.tinify_key;
 const tinify = require("tinify");
+var tinify.key = config.tinify_key;
+
+/*Simon*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/15/171295503-main.jpg");
+source.toFile("roster/slt.jpg");
+
+/*Josh*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/245/171295989-main.jpg");
+source.toFile("roster/lyptica.jpg");
+
+/*Jim*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/231/180224743-main.jpg");
+source.toFile("roster/jimbrews.jpg");
+
+/*Anvarne*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/217/164450265-main.jpg");
+source.toFile("roster/anvarne.jpg");
+
+/*Mikey*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/243/179161843-main.jpg");
+source.toFile("roster/mikeyeast.jpg");
+
+/*Mook*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/131/171116675-main.jpg");
+source.toFile("roster/mookiboi.jpg");
+
+/*Cade*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/150/180372886-main.jpg");
+source.toFile("roster/smomsh.jpg");
+
+/*Cirx*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/97/158009441-main.jpg");
+source.toFile("roster/cirxi.jpg");
+
+/*Orvos*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/115/179313779-main.jpg");
+source.toFile("roster/orvoss.jpg");
+
+/*Nomadian*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/103/178912103-main.jpg");
+source.toFile("roster/nomadian.jpg");
+
+/*Soise*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/214/134145238-main.jpg");
+source.toFile("roster/soise.jpg");
+
+/*Adamwlock*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/112/178428016-main.jpg");
+source.toFile("roster/adamwlock.jpg");
+
+/*Peow*/
+var source = tinify.fromUrl("https://blzmedia-a.akamaihd.net/wow/renders/shadow/hunter-dwarf-male.jpg");
+source.toFile("roster/peow.jpg");
+
+/*Listad*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/187/172653499-main.jpg");
+source.toFile("roster/listad.jpg");
+
+/*Spotz*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/58/178950202-main.jpg");
+source.toFile("roster/spotz.jpg");
+
+/*Kuske*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/159/179323807-main.jpg");
+source.toFile("roster/kuske.jpg");
+
+/*Tom*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/199/171689415-main.jpg");
+source.toFile("roster/tomitaka.jpg");
+
+/*Jinkkz*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/116/179268724-main.jpg");
+source.toFile("roster/jinkkz.jpg");
+
+/*Malidithdh*/
+var source = tinify.fromUrl("https://render-eu.worldofwarcraft.com/character/silvermoon/126/180528766-main.jpg");
+source.toFile("roster/malidithdh.jpg");
