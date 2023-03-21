cf839f4cbb6e2a7883427577cbb6d82f4f7de404
Šimon Soták <simon.sotak@idodo.cz>
2023-03-21 17:47:35 +0100

    Changed contact form

--- contact.php
+++ contact.php
@@ -1,5 +1,63 @@
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    # FIX: Replace this email with recipient email
+    $mail_to = "simonsotak97@gmail.com";
+    
+    # Sender Data
+    $subject = trim($_POST["subject"]);
+    $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $phone - trim($_POST["phone"]);
+    $message = trim($_POST["message"]);
+    
+    if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($message)) {
+        # Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo "Please complete the form and try again.";
+        exit;
+    }
+    
+    # Mail Content
+    $content = "Jméno: $name\n";
+    $content .= "Email: $email\n\n";
+    $content .= "Telefon: $phone \n\n";
+    $content .= "Zpráva:\n$message\n";
+
+    # email headers.
+    $headers = "From: $name <$email>";
+    $headers .= 'MIME-Version: 1.0' ."\r\n";
+    $headers .= 'Content-Type: text/HTML; charset=utf-8' . "\r\n";
+    $headers .= 'Content-Transfer-Encoding: 8bit'. "\n\r\n";
+    $headers .= $text . "\r\n";
+
+
+    # Send the email.
+    $success = mail($mail_to, $subject, $content, $headers);
+    if ($success) {
+        # Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        # Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong, we couldn't send your message.";
+    }
+
+} else {
+    # Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+
+
+
+
+
+
+/*
+
 if ($_POST) {
     $visitor_name = "";
     $visitor_email = "";
@@ -51,5 +109,5 @@ if ($_POST) {
 
 } else {
     echo '<p>Omlouváme se, něco se pokazilo.</p>';
-}
+}*/
 ?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,71 @@
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink');
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink');
+    }
+  };
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+  // Shrink the navbar
+  navbarShrink();
 
-    };
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink);
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector('#mainNav');
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#mainNav',
+      offset: 74,
     });
+  }
 
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
+
+  // Contact form handler
+  $('#submitButton').on('click', function (e) {
+    e.preventDefault();
+
+    var name = $('input#name').val(),
+      email = $('input#email').val(),
+      phone = $('input#phone').val(),
+      message = $('textarea#message').val(),
+      
+      formData =
+        'name=' +
+        name +
+        '&email=' +
+        email +
+        '&phone=' +
+        phone +
+        '&message=' +
+        message;
+
+    $.ajax({
+      type: 'post',
+      url: 'mail.php',
+      data: formData,
+      success: function (results) {
+        $('#successMessage').html(results);
+      },
+    });
+  });
 });
